620f7bc482313d4e88abb77554c0f507ff78c333
BuildTools <267131@student.pwr.edu.pl>
2024-04-10 14:25:27 +0200

    zmiany

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -11,6 +11,268 @@ using namespace std::chrono;
 
 void displayMainMenu(dynamicArray& array, OneWayLinkedListH& listH, OneWayLinkedListHT& listHT, TwoWayLinkedList& list, int& choice, int& number, int& element);
 
+void displayTwoWayLinkedList(TwoWayLinkedList& list, int& choice, int& number, int& element) {
+    cout << "========twoWayLinkedListMENU=======" << endl;
+    cout << "1. Dodaj dane na poczatek listy" << endl;
+    cout << "2. Dodaj dane na koniec listy" << endl;
+    cout << "3. Dodaj dane randomowo do listy" << endl;
+    cout << "4. Usun dane z poczatku listy" << endl;
+    cout << "5. Usun dane z konca listy" << endl;
+    cout << "6. Usun dane randomowo z listy" << endl;
+    cout << "7. Wyszukaj w liscie" << endl;
+    cout << "8. Wyjscie" << endl;
+    cin >> choice;
+    switch (choice) {
+    case 1: {
+        auto start = high_resolution_clock::now();
+        list.addNodeAtBeginning(element);
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayTwoWayLinkedList(list, choice, number, element);
+        break;
+    }
+    case 2: {
+        auto start = high_resolution_clock::now();
+        list.addNodeAtEnd(element);
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayTwoWayLinkedList(list, choice, number, element);
+        break;
+    }
+    case 3: {
+        auto start = high_resolution_clock::now();
+        list.addNodeAtRandomPosition(element, 10);
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayTwoWayLinkedList(list, choice, number, element);
+        break;
+    }
+    case 4: {
+        auto start = high_resolution_clock::now();
+        list.removeNodeAtBeginning();
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayTwoWayLinkedList(list, choice, number, element);
+        break;
+    }
+    case 5: {
+        auto start = high_resolution_clock::now();
+        list.removeNodeAtEnd();
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayTwoWayLinkedList(list, choice, number, element);
+        break;
+    }
+    case 6: {
+        auto start = high_resolution_clock::now();
+        list.removeNodeAtRandomPosition(10);
+        auto stop = high_resolution_clock::now();
+
+        duration<double, milli> duration = stop - start;
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayTwoWayLinkedList(list, choice, number, element);
+        break;
+    }
+    case 7: {
+        int index = rand() % 1000;
+        auto start = high_resolution_clock::now();
+        list.findNode(index);
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayTwoWayLinkedList(list, choice, number, element);
+        break;
+    }
+    case 8: {
+        break;
+    }
+    default:
+        cout << "Nieprawidlowa opcja. Wybierz ponownie" << endl;
+    }
+}
+
+
+void displayOneWayLinkedList(OneWayLinkedListH& listH, OneWayLinkedListHT& listHT, int& choice, int& number, int& element) {
+    cout << "========oneWayLinkedListMENU=======" << endl;
+    cout << "1. Dodaj dane na poczatek listy head" << endl;
+    cout << "2. Dodaj dane na koniec listy head" << endl;
+    cout << "3. Dodaj dane randomowo do listy head" << endl;
+    cout << "4. Usun dane z poczatku listy head" << endl;
+    cout << "5. Usun dane z konca listy head" << endl;
+    cout << "6. Usun dane randomowo z listy head" << endl;
+    cout << "7. Wyszukaj w liscie head" << endl;
+    cout << "----------------------------------" << endl;
+    cout << "8. Dodaj dane na poczatek listy head-tail" << endl;
+    cout << "9. Dodaj dane na koniec listy head-tail" << endl;
+    cout << "10. Dodaj dane randomowo do listy head-tail" << endl;
+    cout << "11. Usun dane z poczatku listy head-tail" << endl;
+    cout << "12. Usun dane z konca listy head-tail" << endl;
+    cout << "13. Usun dane randomowo z listy head-tail" << endl;
+    cout << "14. Wyszukaj w liscie head-tail" << endl;
+    cout << "15. Wyjscie" << endl;
+    cin >> choice;
+    switch (choice) {
+    case 1: {
+        auto start = high_resolution_clock::now();
+        listH.addNodeAtBeginning(element);
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayOneWayLinkedList(listH, listHT, choice, number, element);
+        break;
+    }
+    case 2: {
+        auto start = high_resolution_clock::now();
+        listH.addNodeAtEnd(element);
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayOneWayLinkedList(listH, listHT, choice, number, element);
+        break;
+    }
+    case 3: {
+        auto start = high_resolution_clock::now();
+        listH.addNodeAtRandomPosition(element, 10);
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayOneWayLinkedList(listH, listHT, choice, number, element);
+        break;
+    }
+    case 4: {
+        auto start = high_resolution_clock::now();
+        listH.removeNodeAtBeginning();
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayOneWayLinkedList(listH, listHT, choice, number, element);
+        break;
+    }
+    case 5: {
+        auto start = high_resolution_clock::now();
+        listH.removeNodeAtEnd();
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayOneWayLinkedList(listH, listHT, choice, number, element);
+        break;
+    }
+    case 6: {
+        auto start = high_resolution_clock::now();
+        listH.removeNodeAtRandomPosition(10);
+        auto stop = high_resolution_clock::now();
+
+        duration<double, milli> duration = stop - start;
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayOneWayLinkedList(listH, listHT, choice, number, element);
+        break;
+    }
+    case 7: {
+        int index = rand() % 1000;
+        auto start = high_resolution_clock::now();
+        listH.findNode(index);
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayOneWayLinkedList(listH, listHT, choice, number, element);
+        break;
+    }
+    case 8: {
+        auto start = high_resolution_clock::now();
+        listHT.addNodeAtBeginning(element);
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayOneWayLinkedList(listH, listHT, choice, number, element);
+        break;
+    }
+    case 9: {
+        auto start = high_resolution_clock::now();
+        listHT.addNodeAtEnd(element);
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayOneWayLinkedList(listH, listHT, choice, number, element);
+        break;
+    }
+    case 10: {
+        auto start = high_resolution_clock::now();
+        listHT.addNodeAtRandomPosition(element, 10);
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayOneWayLinkedList(listH, listHT, choice, number, element);
+        break;
+    }
+    case 11: {
+        auto start = high_resolution_clock::now();
+        listHT.removeNodeAtBeginning();
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayOneWayLinkedList(listH, listHT, choice, number, element);
+        break;
+    }
+    case 12: {
+        auto start = high_resolution_clock::now();
+        listHT.removeNodeAtEnd();
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayOneWayLinkedList(listH, listHT, choice, number, element);
+        break;
+    }
+    case 13: {
+        auto start = high_resolution_clock::now();
+        listHT.removeNodeAtRandomPosition(10);
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayOneWayLinkedList(listH, listHT, choice, number, element);
+        break;
+    }
+    case 14: {
+        int index = rand() % 1000;
+        auto start = high_resolution_clock::now();
+        listHT.findNode(index);
+        auto stop = high_resolution_clock::now();
+        duration<double, milli> duration = stop - start;
+
+        cout << "Czas wykonania operacji: " << duration.count() << " milisekund" << endl;
+        displayOneWayLinkedList(listH, listHT, choice, number, element);
+        break;
+    }
+    case 15: {
+        
+        break;
+    }
+    default:
+        cout << "Nieprawidlowa opcja. Wybierz ponownie" << endl;
+    }
+}
 
 void displayArrayMenu(dynamicArray& array, int& choice, int& number, int& element) {
     cout << "========arrayMENU=======" << endl;
@@ -22,7 +284,7 @@ void displayArrayMenu(dynamicArray& array, int& choice, int& number, int& elemen
     cout << "6. Usun dane randomowo" << endl;
     cout << "7. Wyszukaj w tablicy" << endl;
     cout << "8. Wyswietl tablice" << endl;
-    cout << "9. Powrot" << endl;
+    cout << "9. Wyjscie" << endl;
     cin >> choice;
     switch (choice) {
     case 1: {
@@ -102,7 +364,7 @@ void displayArrayMenu(dynamicArray& array, int& choice, int& number, int& elemen
         displayArrayMenu(array, choice, number, element);
         break;
     case 9:
-  
+        
         break;
     default:
         cout << "Nieprawidlowa opcja. Wybierz ponownie" << endl;
@@ -110,14 +372,7 @@ void displayArrayMenu(dynamicArray& array, int& choice, int& number, int& elemen
 }
 
 void displayMainMenu(dynamicArray& array, OneWayLinkedListH& listH, OneWayLinkedListHT& listHT, TwoWayLinkedList& list, int& choice, int& number, int& element) {
-    int randomNumber;
-    for (int i = 0; i < number; i++) {
-        randomNumber = rand();
-        array.addFront(randomNumber);
-        //listH.addNodeAtEnd(randomNumber);
-        //listHT.addNodeAtEnd(randomNumber);
-        //list.addNodeAtEnd(randomNumber);
-    }
+  
     cout << "==========MENU==========" << endl;
     cout << "1. Tablica dynamiczna" << endl;
     cout << "2. Lista jednokierunkowa" << endl;
@@ -130,10 +385,10 @@ void displayMainMenu(dynamicArray& array, OneWayLinkedListH& listH, OneWayLinked
         displayArrayMenu(array, choice, number, element);
         break;
     case 2:
-
+        displayOneWayLinkedList(listH, listHT, choice, number, element);
         break;
     case 3:
-
+        displayTwoWayLinkedList(list, choice, number, element);
         break;
     case 4:
         cout << "Koniec programu" << endl;
@@ -159,6 +414,13 @@ int main()
     srand(seed);
 
     int randomNumber;
+    for (int i = 0; i < number; i++) {
+        randomNumber = rand();
+        array.addFront(randomNumber);
+        listH.addNodeAtEnd(randomNumber);
+        listHT.addNodeAtEnd(randomNumber);
+        list.addNodeAtEnd(randomNumber);
+    }
     
 
     
@@ -168,7 +430,7 @@ int main()
 
 
 
-    //displayMainMenu(array, listH, listHT, list, choice, number, element);
+    displayMainMenu(array, listH, listHT, list, choice, number, element);
 
 
 }
